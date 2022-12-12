04ee3d982831bfaf71b07247d5c546bf3353e57b
Andre <113055933+andre-deveau@users.noreply.github.com>
2022-12-12 14:04:32 -0500

    init

--- js/script.js
+++ js/script.js
@@ -1,23 +1,34 @@
 // Copyright (c) 2022 Andre Deveau All rights reserved
 //
-// Created by: Andre Deveau
+// Created by: Timothy Manwell
 // Created on: Oct 2022
 // This file contains the JS functions for index.html
 
 "use strict"
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Template/sw.js", {
-    scope: "/ICS2O-PWA-Template/",
+  navigator.serviceWorker.register("/ICS2O-Unit-5-03/sw.js", {
+    scope: "/ICS2O-Unit-5-03/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function updates the slider value.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = "Your age: " + valueFromSlider
+
+  if (valueFromSlider > 13) {
+    document.getElementById("movie-rating").innerHTML =
+      "You can see up to an R rated movie."
+  } else if (valueFromSlider == 13) {
+    document.getElementById("movie-rating").innerHTML =
+      "You can see up to a Pg 13 rated movie."
+  } else if (valueFromSlider < 13) {
+    document.getElementById("movie-rating").innerHTML =
+      "You can see up to a G rated movie."
+  }
 }
